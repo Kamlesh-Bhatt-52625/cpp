4d8519743495f718cdf3873235fbf80a71fb597c
Kamlesh Bhatt <89186816+Kamlesh-Bhatt-52625@users.noreply.github.com>
2022-04-28 20:42:14 +0530

    Add files via upload

---
+++ Arrays/9_Pattern_3.cpp
@@ -0,0 +1,19 @@
+//  ANOTHER PROGRAM TO PRINT AN ANOTHER PATTERN
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0; i<=3; i++){
+        for(int j=0; j<=3; j++){
+            if(i>j){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
